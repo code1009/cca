ed3d7c53b6d0db98c2a1facfb959b881c0f0469c
Eui-kwon MOON <code1009@naver.com>
2021-12-07 13:22:17 +0900

    1

--- ut/ut_example.cpp
+++ ut/ut_example.cpp
@@ -5,11 +5,16 @@
 #include "ut_test_2/ut_test_2.hpp"
 #include "ut_test_3/ut_test_my_app.h"
 
+#include <iostream>
+
 
 
 /***************************************************************************/
 int main()
 {
+	// 릴리즈모드 컴파일시 trojan / Win.Generic.C4516440 오탐지 방지;;;
+	std::cout << "Hello World!\n";
+
 	ut_rt_initialize();
 
 	ut_test_1();
